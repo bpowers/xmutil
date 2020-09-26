08b2fa1a8151f31e526ed4c7052f8ab728143df5
Bobby Powers <bobbypowers@gmail.com>
2020-09-26 08:01:01 -0700

    clang-format everything

--- src/Symbol/SymbolNameSpace.h
+++ src/Symbol/SymbolNameSpace.h
@@ -1,42 +1,45 @@
 #ifndef _XMUTIL_SYMBOL_NAMESPACE_H
 #define _XMUTIL_SYMBOL_NAMESPACE_H
-#include <string>
-#include <unordered_map>
 #include <boost/foreach.hpp>
 #include <set>
-class Symbol ;
-class SymbolTableBase ; // forward declaration 
+#include <string>
+#include <unordered_map>
+class Symbol;
+class SymbolTableBase;  // forward declaration
 
-
-
-/* Namespace gives hashed lookup for names 
+/* Namespace gives hashed lookup for names
 
    to make it case and _ insensitive we convert the incoming name before
    passing it to the lookup functions - altering the hash and equality
    function would (likely) be a bit faster */
 
 #define SNSitToSymbol(it) (it.second)
-class SymbolNameSpace
-{
+class SymbolNameSpace {
 public:
-   SymbolNameSpace(void);
-   ~SymbolNameSpace(void);
-   Symbol *Find(const std::string &name) ;
-   void Insert(Symbol *sym) ;
-   bool Remove(Symbol *sym) ;
-   bool Rename(Symbol* sym, const std::string& newname);
-   void DeleteAllUnconfirmedAllocations(void) ;
-   void ConfirmAllAllocations(void) ;
-   void RemoveUnconfirmedAllocation(SymbolTableBase *s) { sUnconfirmedAllocations.erase(s) ; }
-   inline void AddUnconfirmedAllocation(SymbolTableBase *s) { sUnconfirmedAllocations.insert(s) ; }
-   typedef std::unordered_map<std::string, Symbol*> HashTable ;
-   typedef HashTable::value_type iterator ; // allows iterator type to be used directly with BOOST_FOREACH
-   inline HashTable *GetHashTable(void) { return &mHashTable ; } 
+  SymbolNameSpace(void);
+  ~SymbolNameSpace(void);
+  Symbol *Find(const std::string &name);
+  void Insert(Symbol *sym);
+  bool Remove(Symbol *sym);
+  bool Rename(Symbol *sym, const std::string &newname);
+  void DeleteAllUnconfirmedAllocations(void);
+  void ConfirmAllAllocations(void);
+  void RemoveUnconfirmedAllocation(SymbolTableBase *s) {
+    sUnconfirmedAllocations.erase(s);
+  }
+  inline void AddUnconfirmedAllocation(SymbolTableBase *s) {
+    sUnconfirmedAllocations.insert(s);
+  }
+  typedef std::unordered_map<std::string, Symbol *> HashTable;
+  typedef HashTable::value_type iterator;  // allows iterator type to be used directly with BOOST_FOREACH
+  inline HashTable *GetHashTable(void) {
+    return &mHashTable;
+  }
 
-private :
-   std::string *ToLowerSpace(const std::string &name) ;
-   std::set<SymbolTableBase*>sUnconfirmedAllocations ;
-   HashTable mHashTable ;
+private:
+  std::string *ToLowerSpace(const std::string &name);
+  std::set<SymbolTableBase *> sUnconfirmedAllocations;
+  HashTable mHashTable;
 };
 
 #endif
